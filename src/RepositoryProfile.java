7a0e530bb6dfad2bcdcaebd514246a7a2b8f8c1a
sisicity4 <sisicity4@users.noreply.github.com>
2026-08-31 14:07:02 +0900

    class-object-add831

--- src/RepositoryProfile.java
+++ src/RepositoryProfile.java
@@ -1,4 +1,16 @@
-
 public class RepositoryProfile {
-
+	private String name;
+	private int exp;
+	private int level;
+	private int mood;
+	private int hunger;
+	
+	public void gainexp(int amount) {
+		exp += amount;
+		
+		if (exp >= level * 100) {
+			exp -= level * 100;
+			level++;
+		}
+	}
 }
