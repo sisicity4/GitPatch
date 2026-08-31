9f2b3c2998f21677931fdbcf886370cd173f31ba
sisicity4 <sisicity4@users.noreply.github.com>
2026-08-31 14:58:52 +0900

    petとpetserviceの基本的な機能を実装その１

--- src/Pet.java
+++ src/Pet.java
@@ -1,4 +1,57 @@
 
 public class Pet {
+    private String name;
+    private int level;
+    private int exp;
+    private int hunger;
+    private int mood;
+    private String lastCheckedCommitId;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public int getExp() {
+		return exp;
+	}
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+	public int getHunger() {
+		return hunger;
+	}
+	public void setHunger(int hunger) {
+		this.hunger = hunger;
+	}
+	public int getMood() {
+		return mood;
+	}
+	public void setMood(int mood) {
+		this.mood = mood;
+	}
+	public String getLastCheckedCommitId() {
+		return lastCheckedCommitId;
+	}
+	public void setLastCheckedCommitId(String lastCheckedCommitId) {
+		this.lastCheckedCommitId = lastCheckedCommitId;
+	}
+	@Override
+	public String toString() {
+		return lastCheckedCommitId;
 
+	}
+	public Pet(String name) {
+    this.name = name;
+    this.level = 1;
+    this.exp = 0;
+    this.hunger = 50;
+    this.mood = 50;
+	}
 }
--- src/PetService.java
+++ src/PetService.java
@@ -1,4 +1,26 @@
 
 public class PetService {
 
+    private static final int STROKE_MOOD_AMOUNT = 10;
+
+
+	public void gainExperience(Pet pet,int amount) {
+
+     if (amount <= 0) {
+             return;
+         }
+
+    int newExp = pet.getExp() + amount;
+   	pet.setExp(newExp);
+
+	while (pet.getExp() >= pet.getLevel() * 100) {
+	    int requiredExp = pet.getLevel() * 100;
+		pet.setExp(pet.getExp() - requiredExp);
+        pet.setLevel(pet.getLevel() + 1);
+	}
+	}
+	public void feed(Pet pet, int amount){
+
+	}
+
 }
